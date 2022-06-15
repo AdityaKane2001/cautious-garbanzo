be188b6407c7b0dea7f3e275ed5b25d101750c0a
Aditya Kane <64411306+AdityaKane2001@users.noreply.github.com>
2022-06-15 10:43:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hello')
+def pluck(lst, key):
+  return [x.get(key) for x in lst]
+simpsons = [
+  { 'name': 'lisa', 'age': 8 },
+  { 'name': 'homer', 'age': 36 },
+  { 'name': 'marge', 'age': 34 },
+  { 'name': 'bart', 'age': 10 }
+]
+pluck(simpsons, 'age') # [8, 36, 34, 10]
